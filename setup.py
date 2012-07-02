ceca05347c8fdc3b2589a8cc8ec2b75f3ae28943
Markus Törnqvist <mjt@fadconsulting.com>
2012-07-02 17:26:17 +0300

    Setup

---
+++ setup.py
@@ -0,0 +1,36 @@
+# vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
+
+from setuptools import setup, find_packages
+
+NAME = 'mezzanine-captcha'
+VERSION = '0.0.1'
+
+DESCR = """\
+Captcha field for Mezzanine. A hack which can break your fields if you're not careful.
+
+Use with extreme care. Can screw up your field types if you have other apps like this.
+Do not use unless you know what you're doing.
+"""
+
+AUTHOR = u'Markus Törnqvist'
+AUTHOR_EMAIL = 'mjt@fadconsulting.com'
+
+URL = 'https://github.com/mjtorn/mezzanine-captcha'
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCR,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['setuptools'],
+    requires=['django-simple-captcha (>=0.3.5)', 'mezzanine']
+)
+
+# EOF
+
